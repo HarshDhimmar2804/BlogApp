832a0200588586894effc3f2ad1e919d1b056c9c
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-03-25 16:55:08 +0530

    Added OAuth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "blogapp-93649.firebaseapp.com",
+  projectId: "blogapp-93649",
+  storageBucket: "blogapp-93649.appspot.com",
+  messagingSenderId: "273953020041",
+  appId: "1:273953020041:web:3a5e14d186a5eb76af0e58"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
