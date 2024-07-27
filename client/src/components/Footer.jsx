4787ce307344b99a2e2f06121d2aaf08d49ccfc9
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-07-27 13:38:08 +0530

    update.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import { Footer } from "flowbite-react";
-import React from "react";
 import { Link } from "react-router-dom";
 import {
   BsFacebook,
@@ -20,7 +19,7 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Harsh's
+                Harsh&apos;s
               </span>
               Blog
             </Link>
@@ -41,7 +40,7 @@ export default function FooterCom() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Harsh's Blog
+                  Harsh&apos;s Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
