b59bb60963f350af624c0feffb30d1a4e739632c
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-10-10 11:18:02 +0530

    change name of the app

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -19,7 +19,7 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Harsh&apos;s
+              Mighty&apos;s
               </span>
               Blog
             </Link>
@@ -40,7 +40,7 @@ export default function FooterCom() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Harsh&apos;s Blog
+                  Mighty&apos;s Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -70,7 +70,7 @@ export default function FooterCom() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="Harsh's Blog"
+            by="Mighty's Blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
