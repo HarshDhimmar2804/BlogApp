d126ff49a8dab721dc75c97b4363979b4afcac4f
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-10-10 11:38:53 +0530

    add admin functionality to the user

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -46,7 +46,10 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(404, "Invalid password"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
     const { password: pass, ...rest } = validUser._doc;
 
@@ -66,7 +69,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password: pass, ...rest } = user._doc;
       res
         .status(200)
@@ -74,18 +80,21 @@ export const google = async (req, res, next) => {
           httpOnly: true,
         })
         .json(rest);
-    }
-    else{
-      const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+    } else {
+      const generatePassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
       const newUser = new User({
-        username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
       res
         .status(200)
