62f9b8e4f6cdcf7f4493a5abae280baebee590a5
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-11-29 20:08:23 +0530

    complate about and project pages

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Mighty&apos;sBlog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Mighty’s Blog! This blog was created as a personal
+              project to share thoughts, ideas, and experiences with the world.
+              Mighty’s Blog is a space for exploring various topics, ranging
+              from technology and coding to personal growth and creativity.
+            </p>
+            <p>
+              Here, you&apos;ll find regular articles, tutorials, and insights
+              on web development, software engineering, and emerging tech
+              trends. Our goal is to create an engaging and informative platform
+              that inspires learning and innovation.
+            </p>
+            <p>
+              We invite you to leave comments on our posts and connect with
+              other readers. You can like comments, reply to them, and
+              contribute to meaningful discussions. At Mighty’s Blog, we believe
+              that a vibrant community of curious minds can learn and grow
+              together.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Discover fun and engaging projects while exploring topics like HTML,
+        CSS, and JavaScript! Mighty’s Blog is your go-to platform for learning
+        and growing as a developer.
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
