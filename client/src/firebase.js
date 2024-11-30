ca483316848084bb1b1e04ee3f46dabc3ccd289b
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-11-30 09:55:26 +0530

    update firebase file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +10,8 @@ const firebaseConfig = {
   projectId: "blogapp-93649",
   storageBucket: "blogapp-93649.appspot.com",
   messagingSenderId: "273953020041",
-  appId: "1:273953020041:web:3a5e14d186a5eb76af0e58"
-};
+  appId: "1:273953020041:web:3a5e14d186a5eb76af0e58",
+};  
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
